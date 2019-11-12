af22fd2813c1819cad755e7e659061fead73055e
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2019-11-12 16:07:30 +0100

    tp02

---
+++ src/main/java/dev/App.java
@@ -0,0 +1,16 @@
+package dev;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String asciiArt = FigletFont.convertOneLine("App");
+        System.out.println(asciiArt);
+    }
+}
